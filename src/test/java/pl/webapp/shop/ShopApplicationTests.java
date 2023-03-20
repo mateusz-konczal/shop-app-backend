b8bf6702ec1158ae38e2774f8d7d47ec19c3efb5
Mateusz Kończal <mateuszkonczal96@gmail.com>
2023-03-20 23:52:31 +0100

    Initial commit

---
+++ src/test/java/pl/webapp/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.webapp.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
