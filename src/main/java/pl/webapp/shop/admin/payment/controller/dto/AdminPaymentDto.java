b3e39fe08f228b0fc7fc73fb7e6bc5a7feaa60ec
Mateusz Kończal <mateuszkonczal96@gmail.com>
2023-04-18 17:23:11 +0200

    Added payments handling for admin

---
+++ src/main/java/pl/webapp/shop/admin/payment/controller/dto/AdminPaymentDto.java
@@ -0,0 +1,18 @@
+package pl.webapp.shop.admin.payment.controller.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
+import pl.webapp.shop.admin.payment.model.AdminPaymentType;
+
+public record AdminPaymentDto(
+
+        @NotBlank
+        @Length(min = 4)
+        String name,
+
+        AdminPaymentType type,
+
+        boolean defaultPayment,
+
+        String note) {
+}
---
+++ src/main/java/pl/webapp/shop/admin/payment/model/AdminPaymentType.java
@@ -0,0 +1,5 @@
+package pl.webapp.shop.admin.payment.model;
+
+public enum AdminPaymentType {
+    BANK_TRANSFER
+}
