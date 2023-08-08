bb82364178530d18e3bb0b6f6d1d78e53454a7b1
Mateusz Kończal <mateuszkonczal96@gmail.com>
2023-08-08 23:30:16 +0200

    Added the ability to sort products by price

--- src/main/java/pl/webapp/shop/common/repository/ProductRepository.java
+++ src/main/java/pl/webapp/shop/common/repository/ProductRepository.java
@@ -3,6 +3,7 @@ package pl.webapp.shop.common.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pl.webapp.shop.common.model.Product;
 
@@ -14,9 +15,21 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Page<Product> findAllByEnabledIsTrue(Pageable pageable);
 
+    @Query("SELECT p FROM Product p WHERE p.enabled ORDER BY COALESCE(p.salePrice, p.price) ASC")
+    Page<Product> findAllByEnabledIsTrueOrderByCoalesceAsc(Pageable pageable);
+
+    @Query("SELECT p FROM Product p WHERE p.enabled ORDER BY COALESCE(p.salePrice, p.price) DESC")
+    Page<Product> findAllByEnabledIsTrueOrderByCoalesceDesc(Pageable pageable);
+
     Optional<Product> findBySlug(String slug);
 
     Page<Product> findAllByCategoryIdAndEnabledIsTrue(Long categoryId, Pageable pageable);
+
+    @Query("SELECT p FROM Product p WHERE p.categoryId = :categoryId AND p.enabled ORDER BY COALESCE(p.salePrice, p.price) ASC")
+    Page<Product> findAllByCategoryIdAndEnabledIsTrueOrderByCoalesceAsc(Long categoryId, Pageable pageable);
+
+    @Query("SELECT p FROM Product p WHERE p.categoryId = :categoryId AND p.enabled ORDER BY COALESCE(p.salePrice, p.price) DESC")
+    Page<Product> findAllByCategoryIdAndEnabledIsTrueOrderByCoalesceDesc(Long categoryId, Pageable pageable);
 
     List<Product> findTop10BySalePriceIsNotNullAndEnabledIsTrue();
 
--- src/main/java/pl/webapp/shop/product/service/ProductService.java
+++ src/main/java/pl/webapp/shop/product/service/ProductService.java
@@ -2,6 +2,7 @@ package pl.webapp.shop.product.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,7 +24,14 @@ public class ProductService {
     private final ReviewRepository reviewRepository;
 
     public Page<Product> getProducts(Pageable pageable) {
-        return productRepository.findAllByEnabledIsTrue(pageable);
+        String sort = pageable.getSort().toString().toLowerCase();
+        return switch (sort) {
+            case "price: asc" -> productRepository.findAllByEnabledIsTrueOrderByCoalesceAsc(PageRequest.of(
+                    pageable.getPageNumber(), pageable.getPageSize()));
+            case "price: desc" -> productRepository.findAllByEnabledIsTrueOrderByCoalesceDesc(PageRequest.of(
+                    pageable.getPageNumber(), pageable.getPageSize()));
+            default -> productRepository.findAllByEnabledIsTrue(pageable);
+        };
     }
 
     @Transactional(readOnly = true)
